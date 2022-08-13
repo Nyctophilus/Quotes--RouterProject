56338f3f488d1028ddd93e63cf4d0dbc9c0687fe
Medo <mohammed.yuossry@gmail.com>
2022-08-13 11:32:22 +0200

    implemented React.lazy() to split code into chucks

--- src/App.js
+++ src/App.js
@@ -1,39 +1,54 @@
+import { lazy, Suspense } from "react";
 import {
   Routes,
   Route,
   Navigate,
   Link,
 } from "react-router-dom";
-import NotFound from "./pages/NotFound";
-import AllQuotes from "./pages/AllQuotes";
-import NewQuote from "./pages/NewQuote";
-import QuoteDetails from "./pages/QuoteDetails";
 import Layout from "./components/layout/Layout";
-import Comments from "./components/comments/Comments";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+
+const AllQuotes = lazy(() => import("./pages/AllQuotes"));
+const NotFound = lazy(() => import("./pages/NotFound"));
+const NewQuote = lazy(() => import("./pages/NewQuote"));
+const QuoteDetails = lazy(() =>
+  import("./pages/QuoteDetails")
+);
+const Comments = lazy(() =>
+  import("./components/comments/Comments")
+);
 
 function App() {
   return (
     <Layout>
-      <Routes>
-        <Route
-          path="/"
-          element={<Navigate replace to="quotes" />}
-        />
-        <Route path="quotes" element={<AllQuotes />} />
+      <Suspense
+        fallback={
+          <div className="centered">
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <Routes>
+          <Route
+            path="/"
+            element={<Navigate replace to="quotes" />}
+          />
+          <Route path="quotes" element={<AllQuotes />} />
 
-        <Route
-          path="quotes/:id/*"
-          element={<QuoteDetails />}
-        >
-          <Route path="" element={<LoadComments />} />
+          <Route
+            path="quotes/:id/*"
+            element={<QuoteDetails />}
+          >
+            <Route path="" element={<LoadComments />} />
 
-          <Route path="comments" element={<Comments />} />
-        </Route>
+            <Route path="comments" element={<Comments />} />
+          </Route>
 
-        <Route path="new-quote" element={<NewQuote />} />
+          <Route path="new-quote" element={<NewQuote />} />
 
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
     </Layout>
   );
 }
